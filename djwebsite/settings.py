e1b707da66358d7c77653527ad58a7a5f486d5ba
Sofiia <sofiahruba1129@gmail.com>
2024-10-06 01:29:51 +0200

    Edited in lab2

--- djwebsite/settings.py
+++ djwebsite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_blog',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'djwebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djwebs',
+        'USER': 'djwebs',
+        'PASSWORD': '78932100',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
