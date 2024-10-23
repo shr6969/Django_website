b599bd92d00996590f5e818169378f3dfd944999
Sofiia <sofiahruba1129@gmail.com>
2024-10-23 19:04:59 +0200

    Lab 5

--- djwebsite/settings.py
+++ djwebsite/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_DIRS = [
-            os.path.join(BASE_DIR, 'static')
+            os.path.join(BASE_DIR, 'djwebsite/static')
 ]
 
 MEDIA_URL = '/media/'
